29258c526390d8243592036e5c662b294f31db80
prete <martin.prete@sanger.ac.uk>
2021-03-01 15:18:46 +0000

    Update setup.py to get version from celltypist/__init__.py

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 import setuptools
 
-with open("README.md", "rt", encoding="utf-8") as fh:
-    long_description = fh.read()
+def get_readme():
+    with open("README.md", "rt", encoding="utf-8") as fh:
+        return fh.read()
 
-with open("requirements.txt", "rt", encoding="utf-8") as fh:
-    install_requirements = [line.strip() for line in fh.readlines()]
+def get_requirements():
+    with open("requirements.txt", "rt", encoding="utf-8") as fh:
+        return [line.strip() for line in fh.readlines()]
+
+def get_version():
+    with open("celltypist/__init__.py", "rt", encoding="utf-8") as fh:
+        for line in fh.readlines():
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1].strip()
+    raise RuntimeError("Unable to find version string in celltypist/__init__.py")
 
 setuptools.setup(
     name="celltypist-dev",
-    version="0.1.10",
+    version=get_version(),
     author="Tomas Pires de Carvalho Gomes",
     author_email="tpcg@sanger.ac.uk",
     description="A tool for semi-automatic cell type annotation",
-    long_description=long_description,
+    long_description=get_readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/cellgeni/celltypist",
     packages=setuptools.find_packages(),
-    install_requires=install_requirements,
+    install_requires=get_requirements(),
     include_package_data=True,
     entry_points={
         'console_scripts': ['celltypist=celltypist.command_line:main'],
