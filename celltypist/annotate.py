859a10d0436a020fbbff54f9f2438c2fa589aca0
cx1 <cx1@sanger.ac.uk>
2021-03-03 20:28:55 +0000

    add detailed help doc for annotate

--- celltypist/annotate.py
+++ celltypist/annotate.py
@@ -5,7 +5,36 @@ def annotate(filename: str,
              transpose_input: bool = False,
              majority_voting: bool = False,
              over_clustering = None) -> classifier.AnnotationResult:
-    """Run the prediction and (optional) majority voting to annotate the input dataset."""
+    """
+    Run the prediction and (optional) majority voting to annotate the input dataset.
+
+    Parameters
+    ----------
+    filename
+        Path to the input count matrix (supported types are csv, txt, tsv and tab) or Scanpy object (h5ad).
+        If it's the former, a cell-by-gene format is desirable (see `transpose_input` for more information).
+        Genes should be gene symbols. Non-expressed genes are preferred to be provided as well.
+    model
+        Model used to predict the input cells. Default to using the 'Immune_All_Low.pkl' model.
+        To see all available models and their description, use `celltypist.models.models_description()`.
+    transpose_input
+        Whether to transpose the input matrix. Set to `True` if `filename` is provided in a gene-by-cell format.
+        (Default: `False`)
+    majority_voting
+        Whether to refine the predicted labels by running the majority voting classifier after over-clustering.
+        (Default: `False`)
+    over_clustering
+        This argument can be provided in several ways:
+        1) an input plain file with the over-clustering result of one cell per line.
+        2) a string key specifying an existing metadata column in the AnnData (pre-created by the user).
+        3) a python list, numpy array, or pandas series indicating the over-clustering result of all cells.
+        4) if none of the above is provided, will use a heuristic over-clustering approach based on input data size.
+        Ignored if `majority_voting` is set to `False`.
+
+    Returns
+    ----------
+    A `~celltypist.classifier.AnnotationResult` object
+    """
     #load model
     sgd_classifier = models.load(model)
     #construct Classifier class
