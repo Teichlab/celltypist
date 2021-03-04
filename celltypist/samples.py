735e6e00d0110a29ef17f08a558d0615f272c6ac
cx1 <cx1@sanger.ac.uk>
2021-03-04 01:31:51 +0000

    add detailed help doc for get_sample_csv

--- celltypist/samples.py
+++ celltypist/samples.py
@@ -4,10 +4,16 @@ samples_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", 
 
 
 def get_sample_data(filename: str) -> str:
-    """Get path to sample input data included in the package."""
+    """Get the full path to the sample input data included in the package."""
     return os.path.join(samples_path, filename)
 
 
 def get_sample_csv() -> str:
-    """Get path for the sample CSV file included in the package."""
+    """
+    Get the full path to the sample csv file included in the package.
+
+    Returns
+    ----------
+    A string of the full path to the sample csv file (`sample_cell_by_gene.csv`).
+    """
     return get_sample_data("sample_cell_by_gene.csv")
