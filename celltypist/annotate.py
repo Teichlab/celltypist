c3afd8f5a72b4a2b3b09e240c2f95933ec069d11
cx1 <cx1@sanger.ac.uk>
2021-03-07 15:55:29 +0000

    expand over_clustering help doc in annotate

--- celltypist/annotate.py
+++ celltypist/annotate.py
@@ -1,10 +1,13 @@
 from . import classifier, models
+from typing import Optional, Union
+import numpy as np
+import pandas as pd
 
 def annotate(filename: str,
              model: str = "",
              transpose_input: bool = False,
              majority_voting: bool = False,
-             over_clustering = None) -> classifier.AnnotationResult:
+             over_clustering: Optional[Union[str, np.ndarray, pd.Series]] = None) -> classifier.AnnotationResult:
     """
     Run the prediction and (optional) majority voting to annotate the input dataset.
 
