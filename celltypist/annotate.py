670784249543327ec1d3429dfe65922f091a4840
ChuanXu1 <cx1@sanger.ac.uk>
2021-06-10 15:48:34 +0100

    Add docstring for model in annotate

--- celltypist/annotate.py
+++ celltypist/annotate.py
@@ -22,6 +22,8 @@ def annotate(filename: str,
         Genes should be gene symbols. Non-expressed genes are preferred to be provided as well.
     model
         Model used to predict the input cells. Default to using the `Immune_All_Low.pkl` model.
+        Can be a :class:`~celltypist.models.Model` object that wraps the SGDClassifier and the StandardScaler, the
+        path to the desired model file, or the model name.
         To see all available models and their descriptions, use :func:`~celltypist.models.models_description()`.
     transpose_input
         Whether to transpose the input matrix. Set to `True` if `filename` is provided in a gene-by-cell format.
