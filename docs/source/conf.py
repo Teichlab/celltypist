f86a33bb73bdc19a69af2724d0c0bebaabe56630
ChuanXu1 <cx1@sanger.ac.uk>
2022-01-08 00:25:15 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,64 +4,69 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-
 # -- Required imports --------------------------------------------------------
 from datetime import datetime
-
+import celltypist
 
 # -- Project information -----------------------------------------------------
-
 project = 'celltypist'
-author = 'Teichmann Lab'
-copyright = f'{datetime.now():%Y}, {author}.'
-
-
-# The full version, including alpha/beta/rc tags
-release = '0.1.0'
-
-
-# -- General configuration ---------------------------------------------------
+author = 'Chuan Xu, Martin Prete'
+copyright = f'{datetime.now():%Y}, {author}'
+release = celltypist.__version__
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+source_suffix = ['.rst', '.ipynb', '.md']
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-
-
-# -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {'logo_only': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-
+html_static_path = ['_static']
+html_logo = "_static/img/logo_celltypist.png"
+html_favicon = "_static/img/logo_celltypist.png"
 
 # -- Extension configuration -------------------------------------------------
 napoleon_numpy_docstring = True
+
+# Referring to doctrees in other projects
+intersphinx_mapping = dict(
+    python=('https://docs.python.org/3/', None),
+    numpy=('https://docs.scipy.org/doc/numpy/', None),
+    pandas=('https://pandas.pydata.org/pandas-docs/stable/', None),
+    anndata=('https://anndata.readthedocs.io/en/stable/', None),
+    sklearn=('https://scikit-learn.org/stable/', None),
+    scanpy=('https://scanpy.readthedocs.io/en/stable/', None)
+)
