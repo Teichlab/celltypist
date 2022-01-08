32edd9d2b46b15813a93339614fc00ce5ed19af2
ChuanXu1 <cx1@sanger.ac.uk>
2022-01-08 00:37:10 +0000

    Add markdown support in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
