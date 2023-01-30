ab8e4c815ad6c5939889a0db8c27bcd1fab780fd
ChuanXu1 <cx1@sanger.ac.uk>
2023-01-30 11:25:01 +0000

    Add contro

--- celltypist/__init__.py
+++ celltypist/__init__.py
@@ -1,6 +1,13 @@
+#CellTypist 1.0
 from . import classifier, models, samples
 from .annotate import annotate, Model
 from .train import train
 from .plot import dotplot
+#CellTypist 2.0
+from .contro.harmonize import harmonize
+from .contro.integrate import integrate
+from .contro.plot import treeplot
+from .contro.plot import sankeyplot
+from .contro.align import DistanceAlignment
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
