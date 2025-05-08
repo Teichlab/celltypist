33f24c42bb1be605f06f060b69b4844527a0fe61
ChuanXu1 <cx1@sanger.ac.uk>
2025-05-08 21:56:14 +0100

    Detect model path first in command line #151

--- celltypist/command_line.py
+++ celltypist/command_line.py
@@ -69,7 +69,7 @@ def main(indata: str, model: str, transpose_input: bool, gene_file: str, cell_fi
     if model is None:
         model = models.get_default_model()
         logger.info(f"🔖 No model provided. Using the deault: '{model}'")
-    if model not in models.get_all_models() and not os.path.exists(model):
+    if not os.path.isfile(model) and model not in models.get_all_models():
         show_help_and_exit(f"🛑 Invalid model name: '{model}'. Available models are: {', '.join(models.get_all_models())}")
 
     #output dir
