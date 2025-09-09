ce9b5296785c80b63463d2b1638c6b749713715d
ChuanXu1 <cx1@sanger.ac.uk>
2025-09-09 11:13:31 +0100

    Modify get_multilevel_anno

--- celltypist/tree.py
+++ celltypist/tree.py
@@ -1102,7 +1102,7 @@ class Tree():
         for level in range(1, self.depth + 1):
             col_name = f"{prefix}level_{level}_anno"
             mapping = {label: path[min(level - 1, len(path) - 1)].original_name for label, path in label_to_path.items()}
-            results[col_name] = annotations_series.map(mapping)
+            results[col_name] = annotations_series.replace(mapping)
         return pd.DataFrame(results, index = annotations_series.index)
 
 Tree.validate.__doc__ = TreeNode.validate.__doc__.replace("node", "tree")
