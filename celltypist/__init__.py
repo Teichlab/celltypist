0ac85de307004d9e4a24acf7ab0637702be162e5
ChuanXu1 <cx1@sanger.ac.uk>
2025-09-13 22:33:13 +0100

    Update __init__.py

--- celltypist/__init__.py
+++ celltypist/__init__.py
@@ -1,6 +1,6 @@
-from . import classifier, models, samples
+from . import classifier, models, samples, tree
 from .annotate import annotate, Model
 from .train import train
-from .plot import dotplot
+from .plot import dotplot, treeviz
 
 __version__ = "1.7.1"
