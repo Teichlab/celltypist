52ce90b0a60669c0d30af44b5fe30b445b41d9db
ChuanXu1 <cx267@cam.ac.uk>
2025-10-09 20:20:03 +0100

    Export hier_train

--- celltypist/__init__.py
+++ celltypist/__init__.py
@@ -1,6 +1,6 @@
 from . import classifier, models, samples, tree
 from .annotate import annotate, Model
-from .train import train
+from .train import train, hier_train
 from .plot import dotplot, treeviz, treevis
 
 __version__ = "1.7.1"
