00799bf8e14c14c5f32ff0cbf63b5a9d86f7dc98
ChuanXu1 <cx267@cam.ac.uk>
2025-11-13 22:14:52 +0000

    Add reshape (5)

--- celltypist/classifier.py
+++ celltypist/classifier.py
@@ -22,6 +22,8 @@ try:
     import rapids_singlecell as rsc
 except ImportError:
     pass
+NEG_INF = -np.inf
+POS_INF = np.inf
 
 
 class AnnotationResult():
@@ -294,7 +296,7 @@ class HierAnnotationResult():
         filled_labels = self.predicted_labels.ffill(axis = 1, inplace = False)
         decision_by_level = {}
         prob_by_level = {}
-        decision_by_level["level1"] = pd.DataFrame(np.inf, index = filled_labels.index, columns = [self.tree.root.original_name])
+        decision_by_level["level1"] = pd.DataFrame(POS_INF, index = filled_labels.index, columns = [self.tree.root.original_name])
         prob_by_level["level1"] = pd.DataFrame(1.0, index = filled_labels.index, columns = [self.tree.root.original_name])
         for level in range(2, filled_labels.shape[1] + 1):
             parent_labels = filled_labels[f"level{level-1}_predicted_labels"]
@@ -308,7 +310,7 @@ class HierAnnotationResult():
                     child_types = [parent_node.original_name]
                 level_cols.extend(child_types)
 
-            level_decision = pd.DataFrame(-np.inf, index = filled_labels.index, columns = level_cols)
+            level_decision = pd.DataFrame(NEG_INF, index = filled_labels.index, columns = level_cols)
             level_prob = pd.DataFrame(0.0, index = filled_labels.index, columns = level_cols)
             for parent_name in unique_parents:
                 parent_node = self.tree.find_node(parent_name)
@@ -321,7 +323,7 @@ class HierAnnotationResult():
                 else:
                     child_types = [c.original_name for c in parent_node.children if c.size > 0]
                     col = child_types[0] if child_types else parent_node.original_name
-                    level_decision.loc[cell_idx, col] = np.inf
+                    level_decision.loc[cell_idx, col] = POS_INF
                     level_prob.loc[cell_idx, col] = 1.0
 
             decision_by_level[f"level{level}"] = level_decision
@@ -696,7 +698,7 @@ class HierClassifier():
             decision_mats = {}
             prob_mats = {}
             labels["level1_predicted_labels"] = pd.Categorical(np.full(len(self.indata_names), self.model.tree.root.original_name))
-            decision_mats['level1'] = pd.DataFrame(np.inf, index = self.indata_names, columns = [self.model.tree.root.original_name])
+            decision_mats['level1'] = pd.DataFrame(POS_INF, index = self.indata_names, columns = [self.model.tree.root.original_name])
             prob_mats['level1'] = pd.DataFrame(1, index = self.indata_names, columns = [self.model.tree.root.original_name])
             for (level_attr, model), overlap_idx, level_idx in zip(level_classifiers.items(), overlap_idxs, level_idxs):
                 key = level_attr.replace('_classifier', '')
@@ -761,7 +763,7 @@ class HierClassifier():
                     valid_child = [c for c in node.children if c.size > 0][0]
                     logger.info(f"➡️ Passing {len(cell_index)} cells from '{node.original_name}' to its single child '{valid_child.original_name}'")
                     labels.loc[cell_index, f"level{abs_depth+1}_predicted_labels"] = np.full(len(cell_index), valid_child.original_name)
-                    decision_mats[node.original_name] = pd.DataFrame(np.inf, index = cell_index, columns = [valid_child.original_name])
+                    decision_mats[node.original_name] = pd.DataFrame(POS_INF, index = cell_index, columns = [valid_child.original_name])
                     prob_mats[node.original_name] = pd.DataFrame(1, index = cell_index, columns = [valid_child.original_name])
                     _predict_node(valid_child, cell_index)
             _predict_node(self.model.tree.root, self.indata_names)
