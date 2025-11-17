d4d043f78cc8c53cfaa8f17e10438949730db2d6
ChuanXu1 <cx267@cam.ac.uk>
2025-11-17 14:35:30 +0000

    Change order of label_source and over_clustering

--- celltypist/classifier.py
+++ celltypist/classifier.py
@@ -449,17 +449,17 @@ class HierAnnotationResult():
         base += f"\n    adata: AnnData object referred"
         return base
 
-    def majority_vote(self, label_source: str = "refined_labels", over_clustering: Union[list, tuple, np.ndarray, pd.Series, pd.Index], min_prop: float = 0) -> None:
+    def majority_vote(self, over_clustering: Union[list, tuple, np.ndarray, pd.Series, pd.Index], label_source: str = "refined_labels", min_prop: float = 0) -> None:
         """
         Majority vote the celltypist hierarchical predictions using the result from the over-clustering.
 
         Parameters
         ----------
+        over_clustering
+            A list, tuple, numpy array, pandas series or index containing the over-clustering information.
         label_source
             The attribute from which to retrieve cell type labels for majority voting. Must be either `'predicted_labels'` or `'refined_labels'`.
             (Default: `'refined_labels'`)
-        over_clustering
-            A list, tuple, numpy array, pandas series or index containing the over-clustering information.
         min_prop
             For the dominant cell type within a subcluster, the minimum proportion of cells required to support naming of the subcluster by this cell type.
             (Default: 0)
