7e987ac84a30a75ab3668cc2ceb011578220326c
ChuanXu1 <cx267@cam.ac.uk>
2025-12-24 22:15:17 +0000

    Typo

--- celltypist/command_line.py
+++ celltypist/command_line.py
@@ -68,7 +68,7 @@ def main(indata: str, model: str, transpose_input: bool, gene_file: str, cell_fi
     #validate model
     if model is None:
         model = models.get_default_model()
-        logger.info(f"🔖 No model provided. Using the deault: '{model}'")
+        logger.info(f"🔖 No model provided. Using the default: '{model}'")
     if not os.path.isfile(model) and model not in models.get_all_models():
         show_help_and_exit(f"🛑 Invalid model name: '{model}'. Available models are: {', '.join(models.get_all_models())}")
 
--- celltypist/models.py
+++ celltypist/models.py
@@ -93,7 +93,7 @@ class Model():
         """
         if not model:
             model = get_default_model(model_type = 'flat')
-            logger.info(f"🔖 No model provided. Using the deault: '{model}'")
+            logger.info(f"🔖 No model provided. Using the default: '{model}'")
         if '/' not in model and model in get_all_models(model_type = 'flat'):
             model = get_model_path(model)
         if not os.path.isfile(model):
@@ -479,7 +479,7 @@ class HierModel():
         """
         if not model:
             model = get_default_model(model_type = 'hierarchical')
-            logger.info(f"🔖 No model provided. Using the deault: '{model}'")
+            logger.info(f"🔖 No model provided. Using the default: '{model}'")
         if '/' not in model and model in get_all_models(model_type = 'hierarchical'):
             model = get_model_path(model)
         if not os.path.isfile(model):
