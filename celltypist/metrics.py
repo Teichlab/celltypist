edf0bb8bb11e9e4ac63203e8ea587e2246fbee71
ChuanXu1 <cx267@cam.ac.uk>
2026-03-19 19:38:54 +0000

    Add flat_recall

--- celltypist/metrics.py
+++ celltypist/metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from typing import Union, Optional
 from .tree import Tree
 
@@ -82,6 +82,48 @@ def flat_precision(y_true: Union[list, tuple, np.ndarray, pd.Series, pd.Index], 
             raise ValueError(
                     f"🛑 The following labels are not in the tree: {diff}")
     scores = precision_score(y_true, y_pred, labels = labels, average = average, sample_weight = None, zero_division = 0)
+    if average is None:
+        return labels, scores
+    else:
+        return scores
+
+def flat_recall(y_true: Union[list, tuple, np.ndarray, pd.Series, pd.Index], y_pred: Union[list, tuple, np.ndarray, pd.Series, pd.Index], tree: Tree,
+                labels: Optional[Union[list, tuple, np.ndarray, pd.Series, pd.Index]] = None, average: Optional[str] = None) -> Union[tuple, float]:
+    """
+    Compute standard flat classification recall, ignoring hierarchical relationships between cell type labels.
+
+    Parameters
+    ----------
+    y_true
+        Ground-truth labels.
+    y_pred
+        Predicted labels.
+    tree
+        A :class:`~celltypist.tree.Tree` object representing the predefined cell type hierarchy.
+    labels
+        The set of labels to include in the evaluation. Defaults to the unique labels present in `y_true`.
+    average
+        Strategies for recall calculation: `micro` computes recall globally by aggregating TP and FN from all labels, whereas `macro` computes the mean of per-label recall values.
+        If no averaging strategy is specified, recall is calculated individually for each label in `labels`.
+
+    Returns
+    ----------
+    Union[tuple, float]
+        Returns either a tuple containing labels and their corresponding recall values (default), or a single recall value when `average = 'micro'` or `average = 'macro'`.
+    """
+    y_true, y_pred = _check_tree_and_labels(y_true, y_pred, tree)
+    if average not in (None, 'micro', 'macro'):
+        raise ValueError(
+                f"🛑 If specified, `average` must be either `'micro'` or `'macro'`")
+    if labels is None:
+        labels = np.unique(y_true)
+    else:
+        labels = np.asarray(labels)
+        diff = set(labels).difference(tree.cell_types(leaf_only = False))
+        if diff:
+            raise ValueError(
+                    f"🛑 The following labels are not in the tree: {diff}")
+    scores = recall_score(y_true, y_pred, labels = labels, average = average, sample_weight = None, zero_division = 0)
     if average is None:
         return labels, scores
     else:
