bf160596f77fd0fd153d7e8c4f5e894d920be9c6
ChuanXu1 <cx267@cam.ac.uk>
2026-03-21 21:59:28 +0000

    Export metrics

--- celltypist/__init__.py
+++ celltypist/__init__.py
@@ -1,4 +1,4 @@
-from . import classifier, models, samples, tree
+from . import classifier, models, samples, tree, metrics
 from .annotate import annotate, hier_annotate, Model, HierModel
 from .train import train, hier_train
 from .plot import dotplot, treeviz, treevis
