8586afbfc8ee7d92bba9f2a2d1f879abf25072f4
ChuanXu1 <cx267@cam.ac.uk>
2026-05-01 22:24:26 +0100

    Fix sklearn version difference for SGD log_loss (1)

--- celltypist/train.py
+++ celltypist/train.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler,LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn import __version__ as skv
+from packaging import version
 from typing import Optional, Union
 from .models import Model, HierModel
 from . import logger
